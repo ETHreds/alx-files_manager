8f343f02c41d5e4ad19627ec8ca9d34b709f74ce
ETHreds <jcmaglaw@gmail.com>
2024-05-14 17:09:30 +0300

    retry

--- utils/redis.js
+++ utils/redis.js
@@ -1,49 +1,49 @@
 #!/usr/bin/node
 
-const  { createClient } = require('redis');
+const { createClient } = require('redis');
+const { EX } = require('redis');
 
-class RedisClient{
-    constructor(options = {}){
-        this.client  = createClient();
+class RedisClient {
+    constructor(options = {}) {
+        this.client = createClient();
         this.connected = false;
 
         this.client.on('error', (error) => {
             console.error('Redis client error:', error);
-          });
+        });
 
         this.client.on('connect', () => {
-            this.connected = true;})
+            this.connected = true;
+        });
     }
 
-    isAlive () {
+    isAlive() {
         return this.connected;
     }
 
     async get(key) {
         try {
-            const value  = await this.client.get(key)
-
+            const value = await this.client.get(key);
             return value;
         } catch (error) {
-            console.error(error)
-            return -1
-            
+            console.error(error);
+            return -1;
         }
     }
-    
+
     async set(key, value, duration) {
         try {
-        await this.client.set(key, value, EX, duration)
+            await this.client.set(key, value, 'EX', duration);
         } catch (error) {
-            console.error(error)            
+            console.error(error);
         }
     }
 
     async del(key) {
         try {
-            await this.client.del(key)
+            await this.client.del(key);
         } catch (error) {
-            console.error(error)
+            console.error(error);
         }
     }
 }
